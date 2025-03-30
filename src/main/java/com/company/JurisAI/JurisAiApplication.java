3187ae533737630976d38ce1874734f9d20c1495
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-30 14:11:01 +0530

    inital commit  - JurisAI setup

---
+++ src/main/java/com/company/JurisAI/JurisAiApplication.java
@@ -0,0 +1,13 @@
+package com.company.JurisAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurisAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurisAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/JurisAI/JurisAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.JurisAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurisAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
